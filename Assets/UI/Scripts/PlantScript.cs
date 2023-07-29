9a95a67978781ff200379d2eab45fb103038b283
Cristhian Cantillo <crisdcantillo@gmail.com>
2023-07-29 16:45:31 -0500

    ui init and plants script

---
+++ Assets/UI/Scripts/PlantScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantScript : MonoBehaviour
+{
+    public Image pot = null;
+    public Image[] fruits = null;
+
+    void Start()
+    {
+        setFruits(1);
+        setPot(2);
+    }
+
+    public void setFruits(int fruitNumber)
+    {
+        foreach (Image fruit in fruits)
+        {
+            fruit.sprite = Resources.Load<Sprite>("UI/Fruits/" + fruitNumber);
+        }
+    }
+
+    public void setPot(int potNumber)
+    {
+        pot.sprite = Resources.Load<Sprite>("UI/Pots/" + potNumber);
+    }
+}
